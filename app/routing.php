3ed30fb6fa90b1fab1b8f74e3bf789993e70d2ae
Tomasz <tarnawski27@gmail.com>
2017-01-24 21:08:40 +0100

    Add default route

--- app/routing.php
+++ app/routing.php
@@ -1,6 +1,11 @@
 <?php
 
 return  [
+  '/' => [
+      'module'      => 'TaskManager',
+      'controller'  => 'Task',
+      'action'      => 'index'
+  ],
   '/tasks' => [
       'module'      => 'TaskManager',
       'controller'  => 'Task',
